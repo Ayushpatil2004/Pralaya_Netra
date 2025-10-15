943aebf03281326e6742143246383b33405b56f2
Ayushpatil2004 <ayushpatil1364@gmail.com>
2025-10-15 06:22:52 +0530

    Initial Commit

---
+++ client/src/assets/assets.js
@@ -0,0 +1,27 @@
+import arrow_icon from './arrow_icon.png'
+import lock_icon from './lock_icon.svg'
+import logo from './logo.svg'
+import mail_icon from './mail_icon.svg'
+import person_icon from './person_icon.svg'
+import hand_wave from './hand_wave.png'
+import header_img from './header_img.png'
+import pralaya_netra from './pralaya_netra.png'
+import disaster_bg from './disaster_bg.jpg'
+import disaster2_bg from './disaster2_bg.jpg'
+import disaster3_bg from './disaster3_bg.png'
+import disaster4_bg from './disaster4_bg.jpg'
+
+export const assets = {
+    arrow_icon,
+    lock_icon,
+    logo,
+    mail_icon,
+    person_icon,
+    hand_wave,
+    header_img,
+    pralaya_netra,
+    disaster_bg,
+    disaster2_bg,
+    disaster3_bg,
+    disaster4_bg
+}
