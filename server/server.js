2e8d715937876b3287abda4d8317957582671ba8
Ayushpatil2004 <ayushpatil1364@gmail.com>
2025-11-07 20:49:52 +0530

    Final Commit

--- server/server.js
+++ server/server.js
@@ -7,12 +7,18 @@ import connectDB from './config/mongodb.js';
 import authRouter from './routes/authRoutes.js';   
 import userRouter from './routes/userRoutes.js';
 
+import path from 'path';
+
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
 
 const allowedOrigins = ['http://localhost:5173'];
 
+const _dirname = path.resolve();
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -24,10 +30,16 @@ app.use(cors(
 ));
 
 // API Endpoints
-app.get('/', (req, res) => res.send('Server is running'));  
+// app.get('/', (req, res) => res.send('Server is running'));  
   
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
+
+app.use(express.static(path.join(_dirname, "/client/dist")));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT : ${PORT}`);
