9cda6af5d876fec6c6f85992e7e0d8302fdba940
Ayushpatil2004 <ayushpatil1364@gmail.com>
2025-12-03 15:20:34 +0530

    Initial backend setup

--- server/server.js
+++ server/server.js
@@ -1,33 +1,33 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
 
-import connectDB from './config/mongodb.js';
-import authRouter from './routes/authRoutes.js';   
-import userRouter from './routes/userRoutes.js';
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ["http://localhost:5173"];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors(
-  {
-  origin: allowedOrigins,
-  credentials: true
-}
-));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 // API Endpoints
-app.get('/', (req, res) => res.send('Server is running'));  
-  
-app.use('/api/auth', authRouter);
-app.use('/api/user', userRouter);
+app.get("/", (req, res) => res.send("Server is running"));
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT : ${PORT}`);
