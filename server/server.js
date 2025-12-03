9f84e1ca0b154b927be7ca9acc22da8f9856e504
Ayushpatil2004 <ayushpatil1364@gmail.com>
2025-12-03 22:20:53 +0530

    Updated CORS configuration for Render

--- server/server.js
+++ server/server.js
@@ -18,8 +18,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: true,
+    origin: [
+      "https://pralaya-netra-2025.onrender.com",
+      "http://localhost:5173",
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
